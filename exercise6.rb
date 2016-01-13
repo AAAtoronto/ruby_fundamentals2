e1a1ca9aec1281d1a420e7fea6dd6d01f1b9fff2
Arif Alarakhia <dralarakhia@gmail.com>
2016-01-13 16:03:57 -0500

    rice and *

---
+++ exercise6.rb
@@ -0,0 +1,7 @@
+grocery_list = ["bannanas", "appples", "vega bars", "oatmeal", "cookies"]
+
+    grocery_list.push("rice")
+
+        grocery_list.each do |item|
+        puts "* #{item}"
+        end
